87c32d4733e60701d9042adf62e3f1d780718faa
Filip <fipis14@gmail.com>
2020-06-04 13:55:18 +0200

    FInal version of editing comment

--- update.py
+++ update.py
@@ -18,6 +18,10 @@ def update_question(question_id):
 def update_comment(comment_id):
     comment = data_manager.fetch_comments_by_id(comment_id)
     if request.method == "POST":
-        data_manager.update_comment(comment_id)
-        return redirect('/list')
+        data_manager.update_comment(comment)
+        if comment['question_id']:
+            return redirect('/list')
+        else:
+            answer = data_manager.fetch_answers_by_answer_id(comment['answer_id'])
+            return redirect('/question/' + str(answer['question_id']))
     return render_template('modify_data_layout/update_comment.html', comment=comment)
