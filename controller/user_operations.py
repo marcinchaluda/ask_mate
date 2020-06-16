0dbfa6ae856b4f935e921c1859ff30fdc6c02d43
bartcapote <gauclee@gmail.com>
2020-06-16 22:42:55 +0200

    Add salted hashing password protection
    There is a verify_password(<pass>, <hashed_pass>) function ready for use
    in user_operations.py. Returns boolean if passwords match
    
    Move add_new_user() to user_manager
    
    Move add_new_user() to user_manager
    
    Move add_new_user() to user_manager
    
    Move add_new_user() to user_manager

--- controller/user_operations.py
+++ controller/user_operations.py
@@ -1,10 +1,22 @@
 import model.data_manager as data_manager
+import model.user_manager as user_manager
 from flask import render_template, redirect, request, Blueprint, url_for, session
 from psycopg2 import errors
 from model.util import generate_seconds_since_epoch
 from markupsafe import escape
+import bcrypt
 from werkzeug.exceptions import BadRequestKeyError
 user_data = Blueprint('user_operations', __name__)
+
+
+def hash_password(plain_text_password):
+    hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password) -> bool:
+    hashed_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)  # RETURNS TRUE IF PASSWORDS MATCH
 
 
 @user_data.route('/registration', methods=['GET', 'POST'])
@@ -23,13 +35,13 @@ def process_registration():
     new_user_data = {}
     try:
         new_user_data.update({'user_id': request.form['username']})
-        new_user_data.update({'password': request.form['password']})
+        new_user_data.update({'password': hash_password(request.form['password'])})
         new_user_data.update({'user_name': request.form['first_name'] + ' ' + request.form['last_name']})
         new_user_data.update({'submission_time': generate_seconds_since_epoch()})
     except BadRequestKeyError:
         return redirect(url_for('user_operations.show_signup_form'))
     try:
-        data_manager.add_new_user(new_user_data)
+        user_manager.add_new_user(new_user_data)
     except errors.UniqueViolation:
         return redirect(url_for('user_operations.show_signup_form', mail_already_taken=True))
     return redirect(url_for('home'))
