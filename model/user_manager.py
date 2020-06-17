6d1efce02f1d97a48f475ca4c91f8d64a81c9e35
marcinchaluda <chaluda@interia.pl>
2020-06-17 13:44:20 +0200

    fix bug in displaying question text and message

--- model/user_manager.py
+++ model/user_manager.py
@@ -1,6 +1,7 @@
 from model import connection
 from psycopg2.extras import RealDictCursor
 from psycopg2 import sql
+import model.data_manager as data_manger
 
 USERS_HEADERS = ['email', 'user_name', 'registration_time', 'count_of_asked_questions', 'count_of_answers',
                  'count_of_comments', 'reputation']
@@ -22,4 +23,13 @@ def add_new_user(cursor: RealDictCursor, new_user_data):
         submission_time=sql.Literal(new_user_data['submission_time']),
         password=sql.Literal(new_user_data['password'])
     )
+    cursor.execute(query)
+
+
+@connection.connection_handler
+def update_reputation(cursor: RealDictCursor, user_id: str, table_type: str, vote: str):
+    update_vote = data_manger.VOTE_UP if vote == 'vote_up' else data_manger.VOTE_DOWN
+    reputation_points = ""
+    query = sql.SQL('UPDATE new_user SET reputation = reputaion + {reputation_points} WHERE id = {user_id}').\
+        format(reputation_points=sql.Literal(reputation_points), user_id=sql.Literal(user_id))
     cursor.execute(query)
