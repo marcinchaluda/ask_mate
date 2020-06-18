425a566bd07f58de6e78faeba37a034c78d1e77e
Filip <fipis14@gmail.com>
2020-06-18 11:20:33 +0200

    Add bool in answers

--- controller/user_operations.py
+++ controller/user_operations.py
@@ -22,9 +22,10 @@ def login():
         email = request.form['email']
         password = request.form['password']
         user = user_manager.get_user_by_email(email)
-        if email in user['email'] and verify_password(password, user['password']):
-            session['email'] = request.form['email']
-            return redirect(url_for('display_data'))
+        if user is not None:
+            if email in user['email'] and verify_password(password, user['password']):
+                session['email'] = request.form['email']
+                return redirect(url_for('display_data'))
         message = 'Wrong email or password.'
     return render_template('login.html', message=message)
 
